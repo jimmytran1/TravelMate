8b66bd135a63e5635dbebd166da1c628c48662d7
jimmytran1 <jimmytran935@gmail.com>
2024-04-17 11:53:31 -0600

    initial commit

---
+++ src/components/Landing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Landing() {
+  return (
+    <section id="landing">
+        <header>
+            <div className="header__container">
+                <div className="header__description">
+                    <h1>Discover the world's most vibrant travel community platform</h1>
+                    <h2>Connect and share your journey with <span className='orange'>TravelMate</span></h2>
+                </div>
+            </div>
+        </header>
+    </section>
+  )
+}
